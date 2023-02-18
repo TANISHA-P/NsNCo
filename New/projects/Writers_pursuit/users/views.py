937e36c77dfd2b703a7dce7a17bc640b3b392ce7
TANISHA-P <tanishapareek2002@gmail.com>
2023-02-18 15:21:00 +0530

    Functionality added: Home Page user click to navigate to their profile

--- New/projects/Writers_pursuit/users/views.py
+++ New/projects/Writers_pursuit/users/views.py
@@ -26,10 +26,15 @@ def register(request):
 
 @login_required
 def profile(request):
-    # curr_user = user
-    # , {"user_img" : Profile.objects.get(user = user)}
     return render(request,'users/profile.html')
 
 # def adminAccess(request):
 #     if request.user.is_superuser:
-        
+
+def another_person_profile(request, data):
+    the_other_user = User.objects.filter(username = data).first()
+    if(the_other_user):
+        print(the_other_user.email)
+        return render(request,'users/other_profile.html',{"otheruser":the_other_user})
+    else:
+        return render(request,'users/no_user.html')
